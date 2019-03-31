9d49d0bd8127fc6db67683acf9a29c836fb7a81a
Ribauandre <ribauandre@yahoo.com>
2019-03-31 17:42:37 -0400

    adding personal info

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -60,63 +60,71 @@ const Footer = styled.footer`
 const Index = () => (
   <>
     <Layout />
-    <Parallax pages={8}>
+    <Parallax pages={5}>
       <Hero offset={0}>
+      <Avatar src={avatar} alt="John Doe" />
         <BigTitle>
-          Hello, <br /> I'm John Doe.
+          Andre Ribau,
         </BigTitle>
-        <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+        <Subtitle>SRE/SE with a passion for all things tech. 👨‍💻</Subtitle>
       </Hero>
-      <Projects offset={1}>
-        <Title>Projects</Title>
-        <ProjectsWrapper>
-          <ProjectCard
-            title="Freiheit"
-            link="https://www.behance.net/gallery/58937147/Freiheit"
-            bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
-          >
-            This project is my entry to Adobe's #ChallengeYourPerspective contest.
-          </ProjectCard>
-          <ProjectCard
-            title="Harry Potter"
-            link="https://www.behance.net/gallery/52915793/Harry-Potter"
-            bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
-          >
-            I entered the DOCMA 2017 award with this Harry Potter inspired image.
-          </ProjectCard>
-          <ProjectCard
-            title="Tomb Raider"
-            link="https://www.behance.net/gallery/43907099/Tomb-Raider"
-            bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
-          >
-            Recreation of a Tomb Raider Wallpaper (Fan Art)
-          </ProjectCard>
-          <ProjectCard
-            title="Eagle"
-            link="https://www.behance.net/gallery/38068151/Eagle"
-            bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
-          >
-            A fantasy image manipulation relocating the habitat of wild animals.
-          </ProjectCard>
-        </ProjectsWrapper>
-      </Projects>
-      <About offset={3}>
-        <Title>About</Title>
+      <About offset={1}>
+        <Title>Work</Title>
         <AboutHero>
-          <Avatar src={avatar} alt="John Doe" />
           <AboutSub>
-            The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-            Emoji into my speech to better express myself. Winky face.
+          <b>Site Reliability Engineer & Associate Programmer Analyst / Broadridge Financial Solutions
+          </b>
+          <br/><i>Jul 2018 - Current</i>
           </AboutSub>
         </AboutHero>
         <AboutDesc>
-          You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-          every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-          make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The kids
-          want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that all
-          right?
+        Participate in every aspect of the software life cycle from completing sprints to production deployments. Through daily stand up meetings, sprint cycles, and test driven development insuring a high standard of code quality. Work with cross functional teams to meet development and deployment needs, creating an effective CI/CD pipeline, automated testing, and self healing. Developed and implement a REST api (Data Fabric) that is used by multiple high profile clients today
+        </AboutDesc>
+        <br/>
+        <AboutHero>
+          <AboutSub>
+          <b>Java Developer & Associate Programmer Analyst / Broadridge Financial Solutions
+          </b>
+          <br/><i>Jul 2017 - Jul 2018</i>
+          </AboutSub>
+        </AboutHero>
+        <AboutDesc>
+        Led and took part in the design, development, deployment, and support of a full stack company wide management application. (Skillsbank) Troubleshoot real-time production issues to ensure smooth running of impacted application within SLAs. Recommend solutions and identified required program corrections. Deployed production enhancements and analyzed program changes to ensure uninterrupted operation.
         </AboutDesc>
       </About>
+      <Projects offset={2}>
+        <Title>Projects</Title>
+        <ProjectsWrapper>
+          <ProjectCard
+            title="Library-Lender"
+            link="https://thawing-citadel-50331.herokuapp.com/"
+            bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
+          >
+            Uses MongoDB Atlas cloud data-base on an elastic cluster. Node.js Express as the application server. Then React as the front-end technology and Heroku for deployment. This MERN stack was used to create a fast, responsive Library system that allows a user to Donate a book, Borrow/Return a book, or Delete the book. Please feel free to add a book!
+          </ProjectCard>
+          <ProjectCard
+            title="SkillsBank"
+            link=""
+            bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
+          >
+            Utilizes full stack J2EE, AS400, and WebSphere, to deliver an application that allows an associate to rate themselves on many different subjects and gain recognition for what they may know while also allowing managers to identify gaps in skill and lead an associate to develop their skills. Led a team in an agile environment delegating sprints and holding SCRUM meetings to ensure strict deadlines were meet
+          </ProjectCard>
+          <ProjectCard
+            title="BudgetTime.io"
+            link="https://github.com/Ribauandre/BudgetTime.io"
+            bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
+          >
+            A stand alone application that uses MySql as a database, Java in the middle and JavaFX as the front-end. This application allows you to create an account and set categories in which you are allowed to alot a precentage of your net income to. It then takes that information and creates charts and graphs that shows how much of your money goes into each category.
+          </ProjectCard>
+          <ProjectCard
+            title="Region Compare Utility"
+            link=""
+            bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
+          >
+            This utility was made in a full stack JEE for use by the client services department to effectively compare multiple client regions across different LPARS. This is achieved by running as400 commands to create an overview object and then SQL commands to read in the data to java which then quantities and displays the differences between the regions in a simple and user friendly manner.
+          </ProjectCard>
+        </ProjectsWrapper>
+      </Projects>
       <Contact offset={4}>
         <Inner>
           <Title>Get in touch</Title>
