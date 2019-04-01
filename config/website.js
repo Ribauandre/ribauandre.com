e4babdebf080ea542eaa616d6157c282c772b018
git <email@example.com>
2019-03-31 21:58:06 -0400

    adding changes

--- config/website.js
+++ config/website.js
@@ -2,16 +2,15 @@ const tailwind = require('../tailwind')
 
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
-
   siteTitle: 'Andre Ribau | ribauandre.com', // Navigation and Site Title
   siteTitleAlt: 'Andre Ribau', // Alternative Site title for SEO
   siteTitleShort: 'Andre Ribau', // short_name for manifest
   siteHeadline: 'SRE/SE with a passion for all things tech.', // Headline for schema.org JSONLD
-  siteUrl: 'http://ribauandre.com/', // Domain of your site. No trailing slash!
+  siteUrl: 'http://ribauandre.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.jpg', // Used for SEO and manifest
   siteDescription: 'Andre Ribau Personal Website',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  author: 'Andre Ribau', // Author for schema.org JSONLD
 
   // Manifest and Progress color
   themeColor: tailwind.colors.grey,
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -29,7 +29,7 @@ const ProjectsWrapper = styled.div`
   }
   @media (max-width: 900px) {
     grid-template-columns: 1fr;
-    grid-gap: 2rem;
+    grid-gap: 3rem;
   }
 `
 
@@ -60,63 +60,19 @@ const Footer = styled.footer`
 const Index = () => (
   <>
     <Layout />
-    <Parallax pages={5}>
+    <Parallax pages={8}>
       <Hero offset={0}>
-      <Avatar src={avatar} alt="John Doe" />
-        <BigTitle>
-          Andre Ribau,
-        </BigTitle>
-        <Subtitle>SRE/SE with a passion for all things tech 👨‍💻
-        <br/><p><smaller>ribauandre@yahoo.com | (347) 915-3973 </smaller></p>
-
+        <Avatar src={avatar} alt="Andre Ribau" />
+        <BigTitle>Andre Ribau,</BigTitle>
+        <Subtitle>
+          SRE/SE with a passion for all things tech 👨‍💻
+          <br />
+          ribauandre@yahoo.com | (347) 915-3973
+          <br />
         </Subtitle>
       </Hero>
-      <About offset={1}>
-        <Title>Experience</Title>
-        <AboutHero>
-          <AboutSub>
-          <b>Site Reliability Engineer & Associate Programmer Analyst / Broadridge Financial Solutions
-          </b>
-          <br/><i>Jul 2018 - Current</i>
-          </AboutSub>
-        </AboutHero>
-        <AboutDesc>
-        Participate in every aspect of the software life cycle from completing sprints to production deployments. Through daily stand up meetings, sprint cycles, and test driven development insuring a high standard of code quality. Work with cross functional teams to meet development and deployment needs, creating an effective CI/CD pipeline, automated testing, and self healing. Developed and implement a REST api (Data Fabric) that is used by multiple high profile clients today
-        </AboutDesc>
-        <br/>
-        <AboutHero>
-          <AboutSub>
-          <b>Java Developer & Associate Programmer Analyst / Broadridge Financial Solutions
-          </b>
-          <br/><i>Jul 2017 - Jul 2018</i>
-          </AboutSub>
-        </AboutHero>
-        <AboutDesc>
-        Led and took part in the design, development, deployment, and support of a full stack company wide management application. (Skillsbank) Troubleshoot real-time production issues to ensure smooth running of impacted application within SLAs. Recommend solutions and identified required program corrections. Deployed production enhancements and analyzed program changes to ensure uninterrupted operation.
-        </AboutDesc>
-        <br/>
-        <AboutHero>
-          <AboutSub>
-          <b>Software developer / Penn State, The College of IST
-          </b>
-          <br/><i>Aug 2016 - Jan 2017</i>
-          </AboutSub>
-        </AboutHero>
-        <AboutDesc>
-        Worked with a team to debug a system written in Python that is used to rate Teaching Assistants. Released and tested patched versions of the system, and worked with users to better understand needs. Created and partook in team’s project management creating comprehensive plans and deadlines.
-        </AboutDesc>
-        <AboutHero>
-          <AboutSub>
-          <b>Web design & Web management Shadowing / Luce, Schwab & Kase, Inc.
-          </b>
-          <br/><i>May 2013 - Aug 2015</i>
-          </AboutSub>
-        </AboutHero>
-        <AboutDesc>
-        Shadowed Luce, Schwab & Kase, Inc.’s Website manager and developer. (lskair.com) Aided in maintaining the website using HTML/CSS. Helped design Luce, Schwab & Kase, Inc.’s website with interactive user design in mind.
-        </AboutDesc>
-      </About>
-      <Projects offset={2}>
+
+      <Projects offset={1}>
         <Title>Projects</Title>
         <ProjectsWrapper>
           <ProjectCard
@@ -124,44 +80,150 @@ const Index = () => (
             link="https://thawing-citadel-50331.herokuapp.com/"
             bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
           >
-            Uses MongoDB Atlas cloud data-base on an elastic cluster. Node.js Express as the application server. Then React as the front-end technology and Heroku for deployment. This MERN stack was used to create a fast, responsive Library system that allows a user to Donate a book, Borrow/Return a book, or Delete the book. Please feel free to add a book!
+            Uses MongoDB Atlas cloud data-base on an elastic cluster. Node.js Express as the application server. Then
+            React as the front-end technology and Heroku for deployment. This MERN stack was used to create a fast,
+            responsive Library system that allows a user to Donate a book, Borrow/Return a book, or Delete the book.
+            Please feel free to add a book!
           </ProjectCard>
-          <ProjectCard
-            title="SkillsBank"
-            link=""
-            bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
-          >
-            Utilizes full stack J2EE, AS400, and WebSphere, to deliver an application that allows an associate to rate themselves on many different subjects and gain recognition for what they may know while also allowing managers to identify gaps in skill and lead an associate to develop their skills. Led a team in an agile environment delegating sprints and holding SCRUM meetings to ensure strict deadlines were meet
+          <ProjectCard title="SkillsBank" bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)">
+            Utilizes full stack J2EE, AS400, and WebSphere, to deliver an application that allows an associate to rate
+            themselves on many different subjects and gain recognition for what they may know while also allowing
+            managers to identify gaps in skill and lead an associate to develop their skills. Led a team in an agile
+            environment delegating sprints and holding SCRUM meetings to ensure strict deadlines were meet
           </ProjectCard>
           <ProjectCard
             title="BudgetTime.io"
             link="https://github.com/Ribauandre/BudgetTime.io"
             bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
           >
-            A stand alone application that uses MySql as a database, Java in the middle and JavaFX as the front-end. This application allows you to create an account and set categories in which you are allowed to alot a precentage of your net income to. It then takes that information and creates charts and graphs that shows how much of your money goes into each category.
+            A stand alone application that uses MySql as a database, Java in the middle and JavaFX as the front-end.
+            This application allows you to create an account and set categories in which you are allowed to alot a
+            precentage of your net income to. It then takes that information and creates charts and graphs that shows
+            how much of your money goes into each category.
           </ProjectCard>
-          <ProjectCard
-            title="Region Compare Utility"
-            link=""
-            bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
-          >
-            This utility was made in a full stack JEE for use by the client services department to effectively compare multiple client regions across different LPARS. This is achieved by running as400 commands to create an overview object and then SQL commands to read in the data to java which then quantities and displays the differences between the regions in a simple and user friendly manner.
+          <ProjectCard title="Region Compare Utility" bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)">
+            This utility was made in a full stack JEE for use by the client services department to effectively compare
+            multiple client regions across different LPARS. This is achieved by running as400 commands to create an
+            overview object and then SQL commands to read in the data to java which then quantities and displays the
+            differences between the regions in a simple and user friendly manner.
           </ProjectCard>
         </ProjectsWrapper>
       </Projects>
-      <Contact offset={4}>
+
+      <About offset={4}>
+        <Title>Experience</Title>
+        <AboutHero>
+          <AboutSub>
+            <b>Site Reliability Engineer & Associate Programmer Analyst / Broadridge Financial Solutions</b>
+            <br />
+            <i>Jul 2018 - Current</i>
+          </AboutSub>
+        </AboutHero>
+        <AboutDesc>
+          Participate in every aspect of the software life cycle from completing sprints to production deployments.
+          Through daily stand up meetings, sprint cycles, and test driven development insuring a high standard of code
+          quality. Work with cross functional teams to meet development and deployment needs, creating an effective
+          CI/CD pipeline, automated testing, and self healing. Developed and implement a REST api (Data Fabric) that is
+          used by multiple high profile clients today
+        </AboutDesc>
+        <AboutHero>
+          <AboutSub>
+            <b>Java Developer & Associate Programmer Analyst / Broadridge Financial Solutions</b>
+            <br />
+            <i>Jul 2017 - Jul 2018</i>
+          </AboutSub>
+        </AboutHero>
+        <AboutDesc>
+          Led and took part in the design, development, deployment, and support of a full stack company wide management
+          application. (Skillsbank) Troubleshoot real-time production issues to ensure smooth running of impacted
+          application within SLAs. Recommend solutions and identified required program corrections. Deployed production
+          enhancements and analyzed program changes to ensure uninterrupted operation.
+        </AboutDesc>
+        <AboutHero>
+          <AboutSub>
+            <b>Software developer / Penn State, The College of IST</b>
+            <br />
+            <i>Aug 2016 - Jan 2017</i>
+          </AboutSub>
+        </AboutHero>
+        <AboutDesc>
+          Worked with a team to debug a system written in Python that is used to rate Teaching Assistants. Released and
+          tested patched versions of the system, and worked with users to better understand needs. Created and partook
+          in team’s project management creating comprehensive plans and deadlines.
+        </AboutDesc>
+        <AboutHero>
+          <AboutSub>
+            <b>Web design & Web management Shadowing / Luce, Schwab & Kase, Inc.</b>
+            <br />
+            <i>May 2013 - Aug 2015</i>
+          </AboutSub>
+        </AboutHero>
+        <AboutDesc>
+          Shadowed Luce, Schwab & Kase, Inc.’s Website manager and developer. (lskair.com) Aided in maintaining the
+          website using HTML/CSS. Helped design Luce, Schwab & Kase, Inc.’s website with interactive user design in
+          mind.
+        </AboutDesc>
+      </About>
+
+      <Projects offset={5}>
+        <Title>Skills</Title>
+        <ProjectsWrapper>
+          <ProjectCard title="Tech/Methodologies" bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"> 
+            - Amazon Web Services
+            <br/>
+            - Cloud Foundry
+            <br/>
+            - Docker
+            <br/>
+            - Agile
+            <br/>
+            - Scrum
+            <br/>
+            - Continuous Integration
+            <br/>
+            - Test Driven Development
+            <br/>
+            - Jenkins
+            <br/>
+            - Linux
+            <br/>
+            - Nosql (Marklogic, MongoDB)
+            <br/>
+            - RDB (MySql, AS400)
+          </ProjectCard>
+          <ProjectCard title="Code" bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)">
+            - Java
+            <br/>
+            - Python
+            <br/>
+            - JavaScript
+            <br/>
+            - Shell
+            <br/>
+            - Terraform
+            <br/>
+            - Yaml
+            <br/>
+            - Groovy
+            <br/>
+            - HTML, CSS
+            <br/>
+            - Jquery, JSTL
+          </ProjectCard>
+        </ProjectsWrapper>
+      </Projects>
+
+      <Contact offset={7}>
         <Inner>
-          <Title>Get in touch</Title>
+          <Title>Contact Me</Title>
           <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
-            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
-            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
+            Feel free to email me <a href="mailto:ribauandre@yahoo.com">(ribauandre@yahoo.com)</a> or checkout my other links:{' '}
+            <a href="www.linkedin.com/in/ribauandre">LinkedIn</a> &{' '}
+            <a href="https://www.instagram.com/lekoarts.de/">GitHub</a>
           </ContactText>
         </Inner>
         <Footer>
-          &copy; 2019 by Gatsby Starter Portfolio Cara.{' '}
-          <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara">Github Repository</a>. Made by{' '}
-          <a href="https://www.lekoarts.de">LekoArts</a>.
+          <a href="https://github.com/Ribauandre/ribauandre.com">Source Code</a>
         </Footer>
       </Contact>
     </Parallax>
