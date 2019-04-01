2104b478d4f3c4ae8e7c47d1efe8d76a9062a8fa
git <email@example.com>
2019-04-01 12:32:50 -0400

    ert "adding shapes back"
    This reverts commit e173275b3b3c8c71a6dcfd65ccae017a6f4f8a26.

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -21,24 +21,13 @@ const Projects = ({ children, offset }) => (
     <Divider speed={0.1} offset={offset} factor={2}>
       <UpDown>
         <SVG icon="box" width={6} fill={colors.white} left="85%" top="75%" />
-        <SVG icon="upDown" width={8} fill={colors.grey} left="70%" top="20%" />
-        <SVG icon="triangle" width={8} stroke={colors.grey} left="25%" top="5%" />
         <SVG icon="circle" hiddenMobile width={24} fill={colors.white} left="17%" top="60%" />
       </UpDown>
       <UpDownWide>
-        <SVG icon="arrowUp" hiddenMobile width={16} fill={colors.grey} left="20%" top="90%" />
         <SVG icon="triangle" width={12} stroke={colors.white} left="90%" top="30%" />
-        <SVG icon="circle" width={16} fill={colors.grey} left="70%" top="90%" />
-        <SVG icon="triangle" hiddenMobile width={16} stroke={colors.grey} left="18%" top="75%" />
         <SVG icon="circle" width={6} fill={colors.white} left="75%" top="10%" />
-        <SVG icon="upDown" hiddenMobile width={8} fill={colors.grey} left="45%" top="10%" />
       </UpDownWide>
       <SVG icon="circle" width={6} fill={colors.white} left="4%" top="20%" />
-      <SVG icon="circle" width={12} fill={colors.grey} left="80%" top="60%" />
-      <SVG icon="box" width={6} fill={colors.grey} left="10%" top="10%" />
-      <SVG icon="box" width={12} fill={colors.grey} left="29%" top="26%" />
-      <SVG icon="hexa" width={16} stroke={colors.grey} left="75%" top="30%" />
-      <SVG icon="hexa" width={8} stroke={colors.grey} left="80%" top="70%" />
     </Divider>
   </>
 )
